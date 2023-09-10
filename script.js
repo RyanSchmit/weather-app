49b6bfc6d4371f370a45cdc91b8b8090a274cc82
Ryan Schmitt <ryanschmitt@Ryans-Air.home>
2023-09-10 00:12:56 -0400

    Added icon and parsing json data

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 async function getJSON(location) {
     const api_key = '3e21839e546a405eb90200255231408';
-    let input_location = document.getElementById('location').value;
-    input_location = parseLocation(input_location)
-    const base_url = `http://api.weatherapi.com/v1/forecast.json?key=${api_key}&q=${input_location}&days=7`;
+    const base_url = `http://api.weatherapi.com/v1/forecast.json?key=${api_key}&q=${location}&days=7`;
     const response = await fetch(base_url, {mode: 'cors'});
     const data = await response.json()
-    console.log(data)
+    return data
 }
 
 function parseLocation(str) {
@@ -13,3 +11,16 @@ function parseLocation(str) {
     str = str.replaceAll(' ', '-')
     return str
 }
+
+async function getTemps() {
+    let input_location = document.getElementById('location').value;
+    input_location = parseLocation(input_location)
+    const data = await getJSON(input_location)
+    const weather_array = data.forecast.forecastday
+    for (day in weather_array) {
+        console.log(day)
+        console.log(weather_array[day].day.mintemp_f)
+        console.log(weather_array[day].day.maxtemp_f)
+    }
+}
+
