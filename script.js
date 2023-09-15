c01c2eda2889e131069feab0556db4e67eda6b0d
Ryan Schmitt <ryanschmitt@Ryans-Air.home>
2023-09-15 17:18:17 -0400

    Added function to change forecst data

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+class WeatherData {
+    constructor(date, high, low) {
+      this.date = date;
+      this.high = high;
+      this.low = low;
+    }
+  }
+
 async function getJSON(location) {
     const api_key = '3e21839e546a405eb90200255231408';
     const base_url = `http://api.weatherapi.com/v1/forecast.json?key=${api_key}&q=${location}&days=7`;
@@ -12,27 +20,41 @@ function parseLocation(str) {
     return str
 }
 
-async function getTemps() {
-    let input_location = document.getElementById('location').value;
-    input_location = parseLocation(input_location)
+async function parseJSON(start_location) {
+    let input_location = 'new-york';
+    if (start_location == false) {
+        let input_location = document.getElementById('location').value;
+        input_location = parseLocation(input_location)
+    }
     const data = await getJSON(input_location)
     console.log(data)
-    const weather_array = data.forecast.forecastday
-    for (day in weather_array) {
-        console.log(day)
-        console.log(weather_array[day].day.mintemp_f)
-        console.log(weather_array[day].day.maxtemp_f)
+    const forecast = data.forecast.forecastday
+    const weather_array = []
+    for (day in forecast) {
+        weather_array.push(new WeatherData(forecast[day].date, forecast[day].day.mintemp_f, forecast[day].day.maxtemp_f))
     }
+    return weather_array
 }
 
-function changeTemps() {
+async function changeData(start) {
     const dates = document.querySelectorAll("#date");
     const highs = document.querySelectorAll("#high");
     const lows = document.querySelectorAll("#low");
+    let forecast = null;
+    if (start == true) {
+        forecast = await parseJSON(true)
+    }
+    else {
+        forecast = await parseJSON(false)
+    }
+    console.log(forecast)
     for (let i = 0; i < 3; i++) {
-        dates[i].innerHTML = "This is a date";
-        highs[i].innerHTML = "This is a high";
-        lows[i].innerHTML = "This is a low";
+        dates[i].innerHTML = forecast[i].date;
+        highs[i].innerHTML = forecast[i].high;
+        lows[i].innerHTML = forecast[i].low;
     }
 }
+
+changeData(true)
+
 
